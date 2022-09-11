320d3ae3f435a8c1be2ae9750221098cfc515532
Vivek 🧐 <vivekmore@noreply.users.github.com>
2022-09-11 12:09:35 +0530

    🩹 lint-fix 99-print-matrix.js

--- test-integration/scripts/99-print-matrix.js
+++ test-integration/scripts/99-print-matrix.js
@@ -5,32 +5,35 @@ const MATRIX_FILE = 'matrix.json';
 
 let existing = {};
 try {
-  existing = JSON.parse(readFileSync(MATRIX_FILE));
+    existing = JSON.parse(readFileSync(MATRIX_FILE));
 } catch (_) {
-  console.log(`File ${MATRIX_FILE} not found`);
-  existing = { include: [] };
+    // eslint-disable-next-line no-console
+    console.log(`File ${MATRIX_FILE} not found`);
+    existing = { include: [] };
 }
 
 writeFileSync(
-  MATRIX_FILE,
-  JSON.stringify(
-    {
-      include: [
-        ...existing.include,
-        ...process.argv
-          .slice(2)
-          .map(file => {
-            try {
-              return require(`../../${file}`).include;
-            } catch (_) {
-              console.log(`File ${file} not found`);
-              return [];
-            }
-          })
-          .flat(),
-      ],
-    },
-    null,
-    2
-  )
+    MATRIX_FILE,
+    JSON.stringify(
+        {
+            include: [
+                ...existing.include,
+                ...process.argv
+                    .slice(2)
+                    .map((file) => {
+                        try {
+                            // eslint-disable-next-line global-require,import/no-dynamic-require
+                            return require(`../../${file}`).include;
+                        } catch (_) {
+                            // eslint-disable-next-line no-console
+                            console.log(`File ${file} not found`);
+                            return [];
+                        }
+                    })
+                    .flat(),
+            ],
+        },
+        null,
+        2
+    )
 );
